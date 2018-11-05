7608ce0cf7aba1099f7eaf40c6ea60266155f162
Rain-coder <934654902@qq.com>
2018-11-05 18:07:36 +0800

    a

---
+++ 第十四天/dom.js
@@ -0,0 +1,35 @@
+var dom = {
+    children: function () {
+
+    },
+    prev: function () {
+
+    },
+    next: function () {
+
+    },
+    first: function () {
+
+    },
+    last: function () {
+
+    },
+    prevAll: function () {
+
+    },
+    nextAll: function () {
+
+    },
+    index: function () {
+
+    },
+    eq: function () {
+        return app.getElementsByTagName('p')[1]
+    },
+    getClass: function () {
+
+    },
+    sibling: function () {
+
+    }
+}
